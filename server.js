3f7f1e114ffb0e06e9b57e2337b80b3f9a9e3312
aliumair <aliumair662@gmail.com>
2021-08-09 17:27:58 +0500

    updatda

--- server.js
+++ server.js
@@ -454,9 +454,9 @@ app.post("/get_user_list",async function (request,result){
             });*/
             var limit=request.body.limit - list.length;
             if(request.body.grouplist == 1){
-            var query="SELECT message_group.name, message_group.avatar,message_group.id FROM message_group LEFT JOIN message_group_join ON  message_group.id = message_group_join.groupid and message_group_join.user_id='" +users[request.body.username].id+ "' "+groupsearch+"  and message_group.is_community_group=0     GROUP by id limit " +limit+ " ";
+            var query="SELECT message_group.name, message_group.avatar,message_group.id FROM message_group LEFT JOIN message_group_join ON  message_group.id = message_group_join.groupid and message_group_join.user_id='" +users[request.body.username].id+ "' "+groupsearch+"       GROUP by id limit " +limit+ " ";
            if(groupsearch!=''){
-               var query="SELECT message_group.name, message_group.avatar,message_group.id FROM message_group RIGHT JOIN message_group_join ON  message_group.id = message_group_join.groupid and message_group_join.user_id='" +users[request.body.username].id+ "' "+groupsearch+" and message_group.is_community_group=0    GROUP by id limit " +limit+ " ";
+               var query="SELECT message_group.name, message_group.avatar,message_group.id FROM message_group RIGHT JOIN message_group_join ON  message_group.id = message_group_join.groupid and message_group_join.user_id='" +users[request.body.username].id+ "' "+groupsearch+"     GROUP by id limit " +limit+ " ";
            }
             console.log(query);
 
